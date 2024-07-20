53f434111f5c1f07bbdaac22383a734d99ee06dd
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-07-20 14:12:22 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ pd login ubuntu -- yes | apt update && pd login ubuntu -- yes | apt upgrade && p
 pd login ubuntu -- mkdir bedrockserver && pd login ubuntu -- cd bedrockserver
 pd login ubuntu -- wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.21.3.01.zip && pd login ubuntu -- unzip bedrock-server*.zip && pd login ubuntu -- rm -rf bedrock-server*.zip
 pd login ubuntu -- cd ~ && pd login ubuntu -- wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && pd login ubuntu -- wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg && pd login ubuntu -- yes | apt update && pd login ubuntu -- yes | apt install box64-android
-pd login ubuntu -- wget 
+pd login ubuntu -- cd ~ && pd login ubuntu -- wget https://raw.githubusercontent.com/invinc1ble7/bedrockserver/main/server && pd login ubuntu -- mv server ../usr/bin/server && pd login ubuntu -- chmod +x ../usr/bin/server
