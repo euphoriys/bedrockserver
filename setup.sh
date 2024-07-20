75c3db1c3dc040d9d70e453ffbe73b42828971a5
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-07-20 14:49:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 yes | pkg up && yes | pkg install proot-distro
 pd install ubuntu
 echo "pd login ubuntu" >> ../usr/bin/ubuntu && chmod +x ../usr/bin/ubuntu
-pd login ubuntu -- yes | apt update && pd login ubuntu -- yes | apt upgrade && pd login ubuntu -- yes | apt install nano git curl wget gpg
-pd login ubuntu -- mkdir bedrockserver && pd login ubuntu -- cd bedrockserver
-pd login ubuntu -- wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.21.3.01.zip && pd login ubuntu -- unzip bedrock-server*.zip && pd login ubuntu -- rm -rf bedrock-server*.zip
-pd login ubuntu -- cd ~ && pd login ubuntu -- wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && pd login ubuntu -- wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg && pd login ubuntu -- yes | apt update && pd login ubuntu -- yes | apt install box64-android
-pd login ubuntu -- cd ~ && pd login ubuntu -- wget https://raw.githubusercontent.com/invinc1ble7/bedrockserver/main/server && pd login ubuntu -- mv server ../usr/bin/server && pd login ubuntu -- chmod +x ../usr/bin/server
+pd login ubuntu -- bash -c "yes | apt update && yes | apt upgrade && yes | apt install nano git curl wget gpg"
+pd login ubuntu -- bash -c "mkdir bedrockserver"
+pd login ubuntu -- bash -c "cd ~/bedrockserver && wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.21.3.01.zip && unzip bedrock-server*.zip && rm -rf bedrock-server*.zip"
+pd login ubuntu -- bash -c "cd ~ && wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg"
+pd login ubuntu -- bash -c "cd ~ && wget https://raw.githubusercontent.com/invinc1ble7/bedrockserver/main/server && mv server ../usr/bin/server && chmod +x ../usr/bin/server"
