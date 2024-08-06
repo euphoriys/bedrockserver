bc4d7f4b8dca847d8d312af4198046bc865427ee
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-08-06 19:37:20 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 yes | pkg up && yes | pkg install proot-distro
 pd install ubuntu
 echo "pd login ubuntu" >> ../usr/bin/ubuntu && chmod +x ../usr/bin/ubuntu
-pd login ubuntu -- bash -c "yes | apt update && yes | apt upgrade && yes | apt install nano curl wget gpg"
-pd login ubuntu -- bash -c "mkdir bedrockserver"
-pd login ubuntu -- bash -c "cd ~/bedrockserver && wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.21.3.01.zip && unzip bedrock-server*.zip && rm -rf bedrock-server*.zip"
-pd login ubuntu -- bash -c "cd ~ && wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg && yes | apt update && yes | apt install box64-android"
-pd login ubuntu -- bash -c "cd ~ && wget https://raw.githubusercontent.com/invinc1ble7/bedrockserver/main/server && mv server ../usr/bin/server && chmod +x ../usr/bin/server"
+pd login ubuntu -- bash -c '
+yes | apt update && yes | apt upgrade && yes | apt install nano curl wget gpg
+mkdir bedrockserver
+cd ~/bedrockserver && wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.21.3.01.zip && unzip bedrock-server*.zip && rm -rf bedrock-server*.zip
+cd ~ && wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg && yes | apt update && yes | apt install box64-android
+cd ~ && wget https://raw.githubusercontent.com/invinc1ble7/bedrockserver/main/startb && mv startb ../usr/bin/startb && chmod +x ../usr/bin/startb
+cd ~ && wget https://raw.githubusercontent.com/invinc1ble7/bedrockserver/main/autostartb && mv autostartb ../usr/bin/autostartb && chmod +x ../usr/bin/autostartb
+cd ~ && wget https://raw.githubusercontent.com/invinc1ble7/bedrockserver/main/storageb && mv storageb ../usr/bin/storageb && chmod +x ../usr/bin/storageb
+'
