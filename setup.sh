c76df61100eb174afc9f68cab66000e202b1ee32
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-08-06 20:13:03 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-yes | pkg up && yes | pkg install proot-distro
+yes | pkg up && yes | pkg install proot-distro wget
 pd install ubuntu
 echo "pd login ubuntu" >> ../usr/bin/ubuntu && chmod +x ../usr/bin/ubuntu
 pd login ubuntu -- bash -c '
@@ -7,7 +7,6 @@ mkdir bedrockserver
 cd ~/bedrockserver && wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.21.3.01.zip && unzip bedrock-server*.zip && rm -rf bedrock-server*.zip && cd 
 wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg && yes | apt update && yes | apt install box64-android && cd
 '
-yes | pkg install wget
 wget https://raw.githubusercontent.com/invinc1ble7/bedrockserver/main/startb && mv startb ../usr/bin/startb && chmod +x ../usr/bin/startb
 wget https://raw.githubusercontent.com/invinc1ble7/bedrockserver/main/autostartb && mv autostartb ../usr/bin/autostartb && chmod +x ../usr/bin/autostartb
 wget https://raw.githubusercontent.com/invinc1ble7/bedrockserver/main/storageb && mv storageb ../usr/bin/storageb && chmod +x ../usr/bin/storageb
