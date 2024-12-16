4f4ecc91e4a75eb9e2a411e91f6bdcdda8da8c88
euphoriys <93664486+euphoriys@users.noreply.github.com>
2024-12-16 14:15:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "pd login ubuntu" >> ../usr/bin/ubuntu && chmod +x ../usr/bin/ubuntu
 pd login ubuntu -- bash -c '
 yes | apt update && yes | apt upgrade && yes | apt install nano curl wget gpg
 mkdir bedrockserver
-cd ~/bedrockserver && wget https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-1.21.51.02.zip && unzip bedrock-server*.zip && rm -rf bedrock-server*.zip && cd 
+cd ~/bedrockserver && wget https://download1591.mediafire.com/hqz4ic0i1dqgM538qzuLEfc5IKmuz-9h8gNAHFNnvJ7AJieBt69sm-AudIgWrFS6GgTWkQoX6q2k_ChLMptC6wDKcOQuLBbeE9wRgovpeQ_Eu77yQTb_Y4jY1G5TQKIa36Yic-8Zn-L3A-63CS0J4am6j8WYSPLaUhpEIEKkoDGm_w/q8ihgn4v9rpww8t/bedrock-server-1.21.51.02.zip && unzip bedrock-server*.zip && rm -rf bedrock-server*.zip && cd 
 wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg && yes | apt update && yes | apt install box64-android && cd
 '
 wget https://raw.githubusercontent.com/euphoriys/bedrockserver/main/startb && mv startb ../usr/bin/startb && chmod +x ../usr/bin/startb
