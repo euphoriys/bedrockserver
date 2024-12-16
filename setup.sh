2ffe09e1443815c38dc4fab18115ad8c3c17a078
euphoriys <93664486+euphoriys@users.noreply.github.com>
2024-12-16 10:33:27 +0100

    Aktualisieren von setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "pd login ubuntu" >> ../usr/bin/ubuntu && chmod +x ../usr/bin/ubuntu
 pd login ubuntu -- bash -c '
 yes | apt update && yes | apt upgrade && yes | apt install nano curl wget gpg
 mkdir bedrockserver
-cd ~/bedrockserver && wget https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-1.21.44.01.zip && unzip bedrock-server*.zip && rm -rf bedrock-server*.zip && cd 
+cd ~/bedrockserver && wget https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-1.21.51.02.zip && unzip bedrock-server*.zip && rm -rf bedrock-server*.zip && cd 
 wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list && wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg && yes | apt update && yes | apt install box64-android && cd
 '
 wget https://raw.githubusercontent.com/euphoriys/bedrockserver/main/startb && mv startb ../usr/bin/startb && chmod +x ../usr/bin/startb
