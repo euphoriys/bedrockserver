0ebaa1c1afa14e755b8915df2577667b9f4da775
euphoriys <93664486+euphoriys@users.noreply.github.com>
2025-01-01 16:49:06 +0100

    Update minecraft_version.sh

--- src/minecraft_version.sh
+++ src/minecraft_version.sh
@@ -32,12 +32,13 @@ determine_url() {
 
 # Function to download and validate the Bedrock server
 download_and_validate() {
+    local instance_name=$1
     echo "Downloading version: $version"
-    curl -s -A "Mozilla/5.0 (Linux)" -o bedrock-server.zip $url || { echo "Error: Unable to download the specified version."; exit 1; }
+    curl -s -A "Mozilla/5.0 (Linux)" -o "$instance_name.zip" $url || { echo "Error: Unable to download the specified version."; exit 1; }
 
-    if ! unzip -tq bedrock-server.zip > /dev/null 2>&1; then
+    if ! unzip -tq "$instance_name.zip" > /dev/null 2>&1; then
         echo "Error: The specified version does not exist or the downloaded file is not a valid zip file."
-        rm bedrock-server.zip
+        rm "$instance_name.zip"
         exit 1
     fi
 
@@ -94,15 +95,15 @@ done' > autostart.sh
 
 # Function to set up the server
 setup_server() {
+    local instance_name=$1
     echo "Unzipping the downloaded file..."
-    mkdir -p bedrockserver
-    cd bedrockserver
-    unzip -q ../bedrock-server.zip && rm ../bedrock-server.zip
-    cd ~/bedrockserver
+    mkdir -p "$instance_name"
+    cd "$instance_name"
+    unzip -q "../$instance_name.zip" && rm "../$instance_name.zip"
     create_start_script
     create_autostart_script
     echo "Unzipping completed."
-    echo "Setup completed. To start the server, navigate to the 'bedrockserver' directory and run './start.sh'."
+    echo "Setup completed. To start the server, navigate to the '$instance_name' directory and run './start.sh'."
 }
 
 # Function to list existing instances
@@ -111,64 +112,52 @@ list_instances() {
     ls -d bedrockserver_* 2>/dev/null || echo "No instances found."
 }
 
-# Function to replace an existing instance
-replace_instance() {
+# Function to replace the bedrock_server executable in an existing instance
+replace_version() {
     list_instances
-    read -p "Enter the instance number to replace: " instance_number
+    read -p "Enter the instance number to replace the bedrock_server executable: " instance_number
     instance_dir="bedrockserver_$instance_number"
     if [ -d "$instance_dir" ]; then
-        rm -rf "$instance_dir"
-        mv bedrockserver "$instance_dir"
-        echo "Instance $instance_number replaced successfully."
+        unzip -o -j "../$instance_name.zip" "bedrock_server" -d "$instance_dir"
+        rm "../$instance_name.zip"
+        echo "Instance $instance_number updated successfully."
     else
         echo "Instance $instance_number does not exist."
     fi
 }
 
-# Function to create a new instance
-create_new_instance() {
-    read -p "Enter a name for the new instance (leave empty for default naming): " instance_name
-    if [ -z "$instance_name" ]; then
-        instance_count=$(ls -d bedrockserver_* 2>/dev/null | wc -l)
-        if [ "$instance_count" -eq 0 ]; then
-            instance_name="bedrockserver"
-        else
-            instance_number=$((instance_count + 1))
-            instance_name="bedrockserver_$instance_number"
-        fi
-    fi
-    mkdir -p "$instance_name"
-    cd "$instance_name" || { echo "Failed to change directory to $instance_name"; exit 1; }
-    unzip -q ../bedrock-server.zip && rm ../bedrock-server.zip
-    create_start_script
-    create_autostart_script
-    cd ..
-    echo "New instance created: $instance_name"
-}
-
 # Main script execution
-read -p "Do you want to use the latest release, preview, or enter a version manually? [release] " choice
-determine_url "$choice"
-
-download_and_validate
-
 echo "Choose an option:"
 echo "1. Create a new instance"
-echo "2. Replace an existing instance"
+echo "2. Replace the bedrock_server executable in an existing instance"
 echo "3. Overwrite the current instance"
 read -p "Enter your choice [1-3]: " option
 
+read -p "Do you want to use the latest release, preview, or enter a version manually? [release] " choice
+determine_url "$choice"
+
+read -p "Enter a name for the new instance (leave empty for default naming): " instance_name
+if [ -z "$instance_name" ]; then
+    instance_count=$(ls -d bedrockserver_* 2>/dev/null | wc -l)
+    if [ "$instance_count" -eq 0 ]; then
+        instance_name="bedrockserver"
+    else
+        instance_number=$((instance_count + 1))
+        instance_name="bedrockserver_$instance_number"
+    fi
+fi
+
+download_and_validate "$instance_name"
+
 case "$option" in
     1)
-        setup_server
-        create_new_instance
+        setup_server "$instance_name"
         ;;
     2)
-        setup_server
-        replace_instance
+        replace_version "$instance_name"
         ;;
     3)
-        setup_server
+        setup_server "bedrockserver"
         ;;
     *)
         echo "Invalid option."
